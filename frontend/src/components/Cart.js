896c2152cb7cab17645c017659e6118ecb10df9b
calvin chung <calvin.w.chung@gmail.com>
2022-09-15 16:11:18 -0400

    cart page

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,5 +1,97 @@
+import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
+
 const Cart = () => {
-  return( <h2>Cart</h2>)
-}
+  const cart = useSelector(state => state.cart);
+  return(
+    <div className='cart-container'>
+      <h2>Shopping Cart</h2>
+      { cart.cartItems.length === 0 ? (
+        <div className="cart-empty">
+          <p>Your cart is currently empty</p>
+          <div className="start-shopping">
+            <Link to='/'>
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="20" 
+                height="20" 
+                fill="currentColor" 
+                className="bi bi-arrow-left-square" 
+                viewBox="0 0 16 16">
+                <path 
+                  fillRule="evenodd" 
+                  d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+                />
+              </svg>
+              <span>Start Shopping</span>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className="titles">
+            <h3 className="product-title">Products</h3>
+            <h3 className="price">Price</h3>
+            <h3 className="quantity">Quantity</h3>
+            <h3 className="total">Total</h3>
+          </div>
+          <div className="cart-items">
+            {cart.cartItems?.map(cartItem => (
+              <div className="cart-item" key = {cartItem.id}>
+                <div className="cart-product">
+                  <img src={cartItem.image} alt={cartItem.name} />
+                  <div>
+                    <h3>{cartItem.name}</h3>
+                    <p>{cartItem.desc}</p>
+                    <button>Remove</button>
+                  </div>
+                </div>
+                <div className="cart-product-price">
+                  ${cartItem.price}
+                </div>
+                <div className="cart-product-quantity">
+                  <button>-</button>
+                  <div className="count">{cartItem.cartQuantity}</div>
+                  <button>+</button>
+                </div>
+                <div className="cart-product-total-price">
+                  ${cartItem.price * cartItem.cartQuantity}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="cart-summary">
+            <button className="clear-cart">Clear Cart</button>
+            <div className="cart-checkout">
+              <div className="subtotal">
+                <span>Subtotal</span>
+                <span className="amount">${cart.cartTotalAmount}</span>
+              </div>
+              <p>Taxes and shipping calculated at checkout.</p>
+              <button>Checkout</button>
+              <div className="continue-shopping">
+                <Link to='/'>
+                  <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    width="20" 
+                    height="20" 
+                    fill="currentColor" 
+                    className="bi bi-arrow-left-square" 
+                    viewBox="0 0 16 16">
+                    <path 
+                      fillRule="evenodd" 
+                      d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"
+                    />
+                  </svg>
+                  <span>Continue Shopping</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Cart;
